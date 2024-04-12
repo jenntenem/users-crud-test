9641e43aacb7b05e091f1925d2a1e002099c0c58
Jenniffer Tenempaguay <jenntenem@gmail.com>
2024-04-12 14:31:34 -0500

    Modelo de Usuario, cargo y departamento agregados

--- frontend/src/app/modules/users/models/user.model.ts
+++ frontend/src/app/modules/users/models/user.model.ts
@@ -1,5 +1,37 @@
 export enum TipoModificador {
-  Crear,
-  Editar,
-  Eliminar,
+  Crear = "Crear",
+  Editar = "Editar",
+  Eliminar = "Eliminar",
 }
+
+export interface UsuarioBase {
+  id?: number;
+  usuario: string;
+  primerNombre: string;
+  segundoNombre?: string;
+  primerApellido: string;
+  segundoApellido?: string;
+  email?: string;
+  idDepartamento: number;
+  idCargo: number;
+  activo: boolean;
+}
+
+export interface Usuario extends UsuarioBase {
+  nombres?: string;
+  apellidos?: string;
+  cargo?: string;
+  departamento?: string;
+}
+
+interface Administracion {
+  id: number;
+  codigo: string;
+  nombre: string;
+  activo: boolean;
+  idUsuarioCreacion: number;
+}
+
+export interface Departamento extends Administracion {}
+
+export interface Cargo extends Administracion {}
