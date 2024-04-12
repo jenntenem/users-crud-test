7ba65b60e12e2b29efb0f5548201399e5ba53165
Jenniffer Tenempaguay <jenntenem@gmail.com>
2024-04-12 15:16:15 -0500

    Usuario editar y modificar

--- frontend/src/app/modules/users/conponents/user/user.component.ts
+++ frontend/src/app/modules/users/conponents/user/user.component.ts
@@ -1,6 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { TipoModificador } from '@users/models/user.model';
+import {
+  Cargo,
+  Departamento,
+  TipoModificador,
+  Usuario,
+} from '@users/models/user.model';
+import { DepartmentsService } from '@users/services/departments.service';
+import { UsersService } from '@users/services/users.service';
+import { WorkstationsService } from '@users/services/workstations.service';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -11,21 +20,40 @@ export class UserComponent {
   @Output() onHide: EventEmitter<any> = new EventEmitter();
 
   // User
-  @Input() user: any = null;
+  @Input() user: Usuario = null;
   formUser: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  // Dropdown
+  departmentList: Departamento[];
+  workStationList: Cargo[];
+
+  constructor(
+    private fb: FormBuilder,
+    private deptService: DepartmentsService,
+    private wsService: WorkstationsService,
+    private userService: UsersService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
     this.formGroupUser();
+
+    this.deptService.getAllDepartments().subscribe((data) => {
+      this.departmentList = data as Departamento[];
+    });
+
+    this.wsService.getAllWorkStations().subscribe((data) => {
+      this.workStationList = data as Cargo[];
+    });
   }
 
   onHideDialog(value: any = null): void {
-    this.onHide.emit({ onClose: true, value });
+    this.onHide.emit(null);
   }
 
   formGroupUser() {
     this.formUser = this.fb.group({
+      id: [''],
       usuario: [''],
       email: [''],
       primerNombre: ['', [Validators.required]],
@@ -36,8 +64,38 @@ export class UserComponent {
       idCargo: ['', [Validators.required]],
     });
 
-    if (this.user) this.formGroupUser.apply(this.user);
+    if (this.user) this.formUser.patchValue(this.user);
   }
 
+  onSaveUser(): void {
+    const user = this.formUser.value;
+    this.tipo == TipoModificador.Crear
+      ? this.createUser(user)
+      : this.updateUser(user);
+  }
 
+  createUser(user: any): void {
+    delete user.id;
+    this.userService.createUser(user).subscribe((u) => {
+      this.onHideDialog();
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'Usuario Creado',
+        life: 3000,
+      });
+    });
+  }
+
+  updateUser(user: any): void {
+    this.userService.updateUser(user.id, user).subscribe((u) => {
+      this.onHideDialog();
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'Usuario Editado',
+        life: 3000,
+      });
+    });
+  }
 }
